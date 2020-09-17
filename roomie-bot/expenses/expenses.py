f0a7e5f97adb979ffa10596685af5f9b3d5e0c4e
Dipzza <dipzza@protonmail.ch>
2020-09-17 16:46:43 +0200

    feat: :sparkles: Create initial project structure. Close #14

---
+++ roomie-bot/expenses/expenses.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from database import Database
+
+db = Database()
+
+
+def new_payment(chat_id, user, message):
+    pass
+
+
+def get_history(chat_id, message):
+    pass
+
+
+def get_debts(chat_id, user):
+    pass
